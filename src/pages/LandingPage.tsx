50958c0c7580f202467012862dc8fdc44c93b7a3
kavaskar786 <141432964+kavaskar786@users.noreply.github.com>
2025-01-18 20:44:07 +0530

    refactor: enhance LandingPage component with improved loading state handling and animation for articles display

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -8,12 +8,15 @@ import { NoArticle } from "@/components/landingPageComponents/NoArticle";
 import { useCategoryStore } from "@/stores/useCategoryStore";
 import { useLoadingStore } from "@/stores/useLoadingStore";
 import { useResultedArticleStore } from "@/stores/useResultedArticlesStore";
+import { motion } from "framer-motion";
 
 const LandingPage: React.FC = () => {
   const { setArticles } = useArticleStore();
   const { category } = useCategoryStore();
   const { loading, setLoading } = useLoadingStore();
   const { resultedArticles, setResultedArticles } = useResultedArticleStore();
+  const CONTAINER_STYLE =
+    "flex min-h-screen flex-col items-center justify-center";
 
   useEffect(() => {
     const fetchNews = async () => {
@@ -34,18 +37,9 @@ const LandingPage: React.FC = () => {
     fetchNews();
   }, [setArticles, category, setLoading, setResultedArticles]);
 
-  if (resultedArticles.length === 0 && !loading) {
-    return (
-      <div className="flex min-h-screen flex-col items-center justify-center">
-        <SearchComp />
-        <NoArticle />
-      </div>
-    );
-  }
-
   if (resultedArticles && loading) {
     return (
-      <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className={CONTAINER_STYLE}>
         <SearchComp />
         <Loader />
       </div>
@@ -54,19 +48,31 @@ const LandingPage: React.FC = () => {
 
   if (resultedArticles && !loading) {
     return (
-      <div className="flex min-h-screen flex-col items-center justify-center">
+      <div className={CONTAINER_STYLE}>
         <SearchComp />
-        <div className="flex flex-col items-center justify-center">
-          <div className="mt-10 flex min-h-[92vh] flex-wrap items-center justify-center gap-8">
-            {resultedArticles.map((article, index) =>
-              article.title !== "[Removed]" ? (
-                <div key={index}>
-                  <NewsCard Article={article} />
-                </div>
-              ) : null,
-            )}
-          </div>
-        </div>
+        <motion.div
+          className="mt-10 flex min-h-[92vh] flex-wrap items-center justify-center gap-8"
+          initial={{ y: 300 }}
+          animate={{ y: 0 }}
+          transition={{ duration: 0.5 }}
+        >
+          {resultedArticles.map((article, index) =>
+            article.title !== "[Removed]" ? (
+              <div key={index}>
+                <NewsCard Article={article} />
+              </div>
+            ) : null,
+          )}
+        </motion.div>
+      </div>
+    );
+  }
+
+  if (resultedArticles.length === 0 && !loading) {
+    return (
+      <div className={CONTAINER_STYLE}>
+        <SearchComp />
+        <NoArticle />
       </div>
     );
   }
